0a897c62758c0c47acd20c0d4b30c922171f8efe
Surush Azaryun <Surush32@icloud.com>
2025-02-12 14:08:02 -0700

    error in name

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App'; 
-import { name as appName } from './app.json'; 
+import { name as appName } from './app.json';
+const appName = require('./app.json').expo.name;
 
 AppRegistry.registerComponent(appName, () => App); 
